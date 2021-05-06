a2edc2f94f338933f28b403af8f186a11b993084
Ed Beroset <beroset@ieee.org>
2021-05-06 17:29:55 -0400

    Initial working version.

---
+++ src/WANAddr.h
@@ -0,0 +1,84 @@
+#ifndef WANADDR_H
+#define WANADDR_H
+#include <iostream>
+#include <array>
+
+class DMS;
+class Coord;
+
+/*
+ * Format of WANAddr, which is a 48-bit quantity
+ *
+ * 4444 4444 3333 3333 3322 2222 2222 1111 1111 1100 0000 0000
+ * 7654 3210 9876 5432 1098 7654 3210 9876 5432 1098 7654 3210
+ *
+ * Sttt tttt tttt tttt tttt tWgg gggg gggg gggg gggg gggc cccc
+ * ||                        ||                         |
+ * ||                        ||                         +- color
+ * ||                        ||
+ * ||                        |+-- longitude (unsigned), 21 bits
+ * ||                        |
+ * ||                        +--- 1 = W, 0 = E longitude
+ * ||
+ * |+-- latitude (unsigned), 20 bits
+ * |
+ * +--- 1 = S, 0 = N latitude
+ * 
+ * Latitude and longitude are scaled such that a full scale 
+ * 180 degree value is 0x20'0000 (i.e. 21 bits)
+ *
+ * Color is used to distinguish multiple nodes at the same location.
+ * 
+ */
+struct WANAddr : public std::array<uint8_t, 6> {
+    friend std::ostream& operator<<(std::ostream& out, const WANAddr& addr);
+    Coord getLat() const;
+    Coord getLong() const;
+    uint8_t getColor() const;
+    DMS toDMS() const;
+};
+
+class Coord {
+public:
+    Coord(uint8_t degrees, uint8_t minutes, uint8_t seconds, uint16_t milliseconds, bool NE);
+    Coord(double a);
+
+    friend std::ostream& operator<<(std::ostream& out, const Coord& coord);
+    double asDecimal() const { return a; } 
+    double operator()(double) const { return a; }
+    // these only have 21 significant bits, 
+    // so any difference less than that is essentially zero
+    static constexpr double epsilon{1.0 / 0x20'0000};
+private:
+    double a;
+};
+
+inline bool operator==(const Coord& lhs, const Coord& rhs) { 
+    return (lhs.asDecimal() - rhs.asDecimal()) < Coord::epsilon;
+}
+
+class DMS {
+public:
+    DMS(Coord a, Coord b, uint8_t color);
+    WANAddr asWANAddr() const;
+    friend std::ostream& operator<<(std::ostream& out, const DMS& dms);
+    friend bool operator==(const DMS& lhs, const DMS& rhs);
+    Coord getLat() const { return latitude; }
+    Coord getLong() const { return longitude; }
+    static constexpr double anglefactor{0x20'0001 / 180.0};
+    static constexpr double invanglefactor{-180.0 / 0x20'0000};
+    static constexpr uint8_t colormask{0x1f};
+private:
+    Coord latitude;
+    Coord longitude;
+    uint8_t color;
+};
+
+inline bool operator==(const DMS& lhs, const DMS& rhs){ 
+    return lhs.latitude == rhs.latitude 
+        && lhs.longitude == rhs.longitude
+        && lhs.color == rhs.color;
+}
+inline bool operator!=(const DMS& lhs, const DMS& rhs){return !operator==(lhs,rhs);}
+
+#endif // WANADDR_H
---
+++ src/main.cpp
@@ -0,0 +1,32 @@
+#include "WANAddr.h"
+#include <iostream>
+#include <iomanip>
+#include <vector>
+#include <utility>
+
+int main()
+{
+    std::vector<std::pair<DMS, WANAddr>> samples{ 
+        { {{ 88, 56, 47, 451, false}, {  2, 33, 30, 388, true }, 0},    { 0xFE, 0x80, 0x70, 0x0e, 0x8E, 0x00} },
+        { {{ 88, 56, 47, 451, false}, { 10, 42, 37,  68, true }, 0},    { 0xFE, 0x80, 0x70, 0x3c, 0xee, 0x00} },
+        { {{ 88, 56, 47, 451, false}, {135, 13, 13, 488, true }, 0},    { 0xFE, 0x80, 0x73, 0x01, 0x41, 0x00} },
+        { {{ 34,  2, 56, 832, true }, { 84, 15, 59, 745, false}, 0},    { 0x4F, 0x93, 0x1E, 0x20, 0x9d, 0xe0} },
+        { {{ 35,  4, 59, 858, true }, {160, 10,  0,  25, false}, 0},    { 0x4e, 0x1a, 0x94, 0x70, 0xd4, 0x60} },
+        { {{ 35, 14, 59, 918, true }, {159, 59, 59, 966, false}, 0},    { 0x4d, 0xdd, 0xe4, 0x71, 0xc7, 0x20} },
+        { {{ 35, 19, 59, 947, true }, {160, 20,  0,  85, false}, 0},    { 0x4d, 0xbf, 0x8c, 0x6f, 0xe1, 0xa0} },
+        { {{ 46, 36, 15, 650, true }, { 94, 16,  7, 641, false}, 2},    { 0x3d, 0xb7, 0xe5, 0xe7, 0xb7, 0x22} },
+        { { 33.179569 , -96.698913, 0},                                 { 0x50, 0xcf, 0xb5, 0xD9, 0xE4, 0x00} }, 
+        { { 33.1763075, -96.699600, 0},                                 { 0x50, 0xD0, 0xE5, 0xD9, 0xE3, 0x00} }, 
+    };
+    
+    for (const auto [coord, addr] : samples) {
+        std::cout << addr  << '\t' << coord << "\t( " 
+            << std::setprecision(9) << coord.getLat().asDecimal() << ", " << coord.getLong().asDecimal() << ")\n";
+        const auto calc = coord.asWANAddr();
+        if (calc != addr) 
+            std::cout << "\tCalculated " << calc << '\n';
+        const auto calcDMS = addr.toDMS();
+        if (calcDMS != coord) 
+            std::cout << "\tCalculated\t" << calcDMS << '\n';
+    }
+}
